581c0a6bbf31d4212640de4f4144359d277586c6
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-09-22 00:19:29 +0700

    build

--- app/scripts/ui-kit.js
+++ app/scripts/ui-kit.js
@@ -1,59 +1,10 @@
 import Modal from './components/modal'
 import Navbar from './components/navbar'
-import Slider from './components/slider'
 import Tab from './components/tab'
 
 document.addEventListener('DOMContentLoaded', function() {
-
-  function contructor() {
-    Modal.loaded()
-    Navbar.loaded()
-    Tab.loaded()
-
-    DetectTouchDevice()
-
-    let slider = new Slider('.uikit-slider')
-  }
-
-  window.addEventListener('resize', function() {
-    DetectTouchDevice()
-  })
-
-  let modal = document.querySelector('.modal')
-  modal.addEventListener('modal.show', function() {
-    console.log('show')
-  })
-  modal.addEventListener('modal.shown', function() {
-    console.log('shown')
-  })
-  modal.addEventListener('modal.hide', function() {
-    console.log('hide')
-  })
-  modal.addEventListener('modal.hidden', function() {
-    console.log('hidden')
-  })
-
-  contructor()
+  
+  Modal.loaded()
+  Navbar.loaded()
+  Tab.loaded()
 })
-
-function DetectTouchDevice() {
-  let html = document.querySelector('html')
-
-  if(navigator.userAgent.match(/(iPhone|iPod|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i)) {
-    html.classList.add('touch-device')
-  }
-  else {
-    html.classList.remove('touch-device')
-  }
-}
-//
-// function DetectFullscreen() {
-//   let html = document.querySelector('html')
-//
-//   if(html.classList.contains('touch-device')) {
-//     let elements = document.querySelectorAll('.fullscreen')
-//     elements.forEach(function(element) {
-//       element.style.height = `${window.innerHeight}px`
-//     })
-//   }
-// }
